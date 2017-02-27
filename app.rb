911e4b4f1a5f76cf66f035173ffdfdb029038df5
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-27 22:06:09 +0000

    added views directory and index.erb

--- app.rb
+++ app.rb
@@ -1,19 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "only special people will see this..."
-end
-
-get '/social' do
-  "some rich media here would be nice..don't you think?"
-end
-
 get '/cat' do
-  "<div>
-    <img style='border: 3px dashed red' src='http://bit.ly/1eze8aE' />
-   </div>"
+  erb(:index)
 end
