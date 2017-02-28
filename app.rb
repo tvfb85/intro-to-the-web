528957e0ce03d3455c4878facfec2070c9d0e009
Ryan <Ryanchu.ws@gmail.com>
2017-02-28 15:17:26 +0000

    posted params

--- app.rb
+++ app.rb
@@ -2,15 +2,19 @@ require 'sinatra'
 
 set :session_secret, 'super secret'
 
-get '/random-cat' do
+post '/random-cat' do
   #instance variable has to be declared first
   @name = %w(Amigo Oscar Viking).sample
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   @age = params[:age]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:form)
 end
