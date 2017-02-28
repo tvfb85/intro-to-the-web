d235c500dcf722f9861d142fb3d3a36b98d881d7
Vicky Roberts <vicki_uk@hotmail.com>
2017-02-28 14:05:39 +0000

    syntax for params added

--- app.rb
+++ app.rb
@@ -1,7 +1,15 @@
 require 'sinatra'
 
-get '/cat' do
+set :session_secret, 'super secret'
+
+get '/random-cat' do
   #instance variable has to be declared first
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
